1194cc7ebd0fa6c34bcbce623d8e626793f65fce
Rebecca <rggelles@gmail.com>
2023-12-12 16:25:54 -0500

    Switch to monthly updates, production Airflow

--- push_to_airflow.sh
+++ push_to_airflow.sh
@@ -1,8 +1,8 @@
-gsutil cp huggingface_dag.py gs://us-east1-dev2023-cc1-b088c7e1-bucket/dags/
-gsutil cp sequences/* gs://us-east1-dev2023-cc1-b088c7e1-bucket/dags/sequences/huggingface/
-gsutil rm gs://us-east1-dev2023-cc1-b088c7e1-bucket/dags/sql/huggingface/*
-gsutil cp sql/* gs://us-east1-dev2023-cc1-b088c7e1-bucket/dags/sql/huggingface/
-gsutil cp schemas/* gs://us-east1-dev2023-cc1-b088c7e1-bucket/dags/schemas/huggingface/
+gsutil cp huggingface_dag.py gs://us-east1-production2023-cc1-01d75926-bucket/dags/
+gsutil cp sequences/* gs://us-east1-production2023-cc1-01d75926-bucket/dags/sequences/huggingface/
+gsutil rm gs://us-east1-production2023-cc1-01d75926-bucket/dags/sql/huggingface/*
+gsutil cp sql/* gs://us-east1-production2023-cc1-01d75926-bucket/dags/sql/huggingface/
+gsutil cp schemas/* gs://us-east1-production2023-cc1-01d75926-bucket/dags/schemas/huggingface/
 gsutil rm -r gs://airflow-data-exchange/huggingface/schemas/*
 gsutil cp schemas/* gs://airflow-data-exchange/huggingface/schemas/
-gsutil -m cp -r huggingface_scripts/* gs://us-east1-dev2023-cc1-b088c7e1-bucket/dags/huggingface_scripts/
+gsutil -m cp -r huggingface_scripts/* gs://us-east1-production2023-cc1-01d75926-bucket/dags/huggingface_scripts/
